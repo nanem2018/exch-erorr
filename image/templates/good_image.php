b24c83b7688c8c3a630987495fa885392da4c3b8
1234567virt <44651105+1234567virt@users.noreply.github.com>
2019-11-11 15:50:36 +0300

    error-fix3
    закрывать соединения с базой над в конце.И у тебя неправильный индентифекатор БД был (db) сверху ,а снизу ($bd)

--- image/templates/good_image.php
+++ image/templates/good_image.php
@@ -1,13 +1,15 @@
 <?php
-$db=mysqli_connect('localhost:3306','root','123456','lesson_6') or die('No connect');
+$db=mysqli_connect('localhost','root','123456','lesson_6') or die('No connect');
 $query='SELECT * FROM main';
 $result=mysqli_query($db,$query);
 
 
-mysqli_close($bd);
-var_dump($result);
+
   ?>   
 <?php $myArray =["1","2", "3","4","5","6"]?>
  //<?php foreach ($myArray as $value):?>
                   <a href="./public/img/images/<?=$value?>.jpg"><img alt="image" src="./public/img/images_small/<?=$value?>.jpg"></a>  
-    <?php endforeach;?>   
+    <?php endforeach;
+
+mysqli_close($db);
+?>   
